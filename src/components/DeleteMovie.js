dccf967e4cc9ebdcd5520372374b054e540f079d
Arpita Maurya <aditimaurya1234@gmail.com>
2021-07-06 11:20:04 +0530

    created reusable component for input

--- src/components/DeleteMovie.js
+++ src/components/DeleteMovie.js
@@ -1,36 +1,52 @@
 import React, { Component } from 'react'
 import { Button, Form } from 'react-bootstrap';
+import InputField from './inputField';
 
 class DeleteMovie extends Component {
     constructor(props) {
-        super(props)
-    
+        super(props);
+
         this.state = {
              movieName:''
         }
     }
 
- handleName=(event)=>{
+ handleChange=(event)=>{
         this.setState({
             movieName:event.target.value
         })
+
     }
-    
+handleSubmit =e=>
+{
+    e.preventDefault();
+    console.log(this.state);
+}
     render() {
+        const {movieName}= this.state;
         return (
             <div className='container'>
                 <div className="jumbotron my-5">
+                     <form onSubmit={this.handleSubmit}>
                     <div>
-                    <Form.Group controlId="formBasicEmail">
+                    {/* <Form.Group controlId="formBasicEmail">
                     <Form.Label>Movie Name</Form.Label>
-                    <Form.Control type="text" placeholder="movie name" value={this.state.movieName} onChange={this.handleName} />
-                </Form.Group>
+                    <Form.Control type="text" placeholder="movie name" value={movieName} onChange={this.handleChange} />
+                </Form.Group> */}
+                <InputField               
+                 type="text"
+         value={this.state.movieName}
+         placeholder="movie Name"
+         label="Movie Name"
+         name="movieName"
+         onChange={this.handleChange}/>
                     </div>
                     <div  className="App">
                     <Button variant="danger" type="submit">Delete it</Button>{' '}
-                    </div>
+                    </div> 
+                    </form>
                 </div>
-                
+               
             </div>
         )
     }
